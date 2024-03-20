8750b4def7fa55aee0ff9c8d406e8bea7ce76c10
nuk3dev <118739856+nuk3dev@users.noreply.github.com>
2024-03-20 11:52:20 +0100

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller {
-    public function index(Request $request) {
+    public function index() {
+
+        $player = DB::table('player')
+                ->orderBy('highestscore', 'desc')
+                ->limit(5)
+                ->get();
+        return view('welcome')->with(['players' => $player]);
+    }
+
+    public function InsertAjaxData(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'secondsplayed' => 'required|numeric',
+        ]);
+    
+        $data = [
+            'name' => $request->input('username'),
+            'highestScore' => $request->input('secondsplayed'),
+        ];
+    
+        DB::table('player')->insert($data);
+
+        return response()->json(['success' => true]);
     }
 }
+
+
 
